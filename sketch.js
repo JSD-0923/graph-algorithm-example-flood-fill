d015fed8e08fa4061869ae6ce2d5cf40dbc81380
Hartha Aloufi <hartha.aloufee@econstruct.ai>
2023-09-26 21:48:05 +0300

    Feat: create basic p5js skelaton

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Define variables and setup function
+function setup() {
+    // Create a canvas of a specific size
+    createCanvas(400, 400);
+    // Set background color or other setup configurations
+}
+
+// Main drawing function
+function draw() {
+    // Your drawing and interaction code goes here
+}
+
+// Function to handle mouse click event
+function mouseClicked() {
+    // Implement your Flood Fill logic here when the mouse is clicked
+}
