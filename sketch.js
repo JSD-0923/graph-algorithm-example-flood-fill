facc5967677c5292321d6db49e8a7d56ecc6c133
Hartha Aloufi <hartha.aloufee@econstruct.ai>
2023-09-27 00:40:22 +0300

    feat: implement flood fill

--- sketch.js
+++ sketch.js
@@ -1,26 +1,27 @@
-let img;
+// img for the image, visited
+let img, visited;
 
 function setup() {
-    createCanvas(400, 400);
+    // create canvas
+    createCanvas(800, 800);
+
+    // attach loadImage to the file input
     let fileInput = select('#imageUpload');
     fileInput.changed(handleImageUpload);
 }
 
-function draw() {
-}
 
 function mouseClicked(evt) {
     if (evt.target.id == 'defaultCanvas0') {
-        // Flood fill at mouse position
-        let x = mouseX;
-        let y = mouseY;
 
-        ellipse(x, y, 10, 10);
+        // flood fill with purple from the mouse position
+        floodFill(mouseX, mouseY, [255, 0, 255, 255]);
 
         // prevent default
         return false;
     }
 }
+
 
 function handleImageUpload() {
     let file = select('#imageUpload').elt.files[0];
@@ -33,3 +34,65 @@ function handleImageUpload() {
         alert('Please select a valid image file.');
     }
 }
+
+
+/**
+ * Check if the two colors are the same by comparing their RGBA values
+ */
+const isSameColor = (color1, color2) => {
+    return color1[0] === color2[0] && color1[1] === color2[1] && color1[2] === color2[2] && color1[3] === color2[3];
+}
+
+function createVisitedArray() {
+    let arr = []
+
+    for (let i = 0; i < 800; i++) {
+        arr[i] = []
+        for (let j = 0; j < 800; j++) {
+            arr[i][j] = false;
+        }
+    }
+
+    return arr;
+}
+
+
+/**
+ * Flood fill algorithm
+ */
+const floodFill = (x, y, fillColor) => {
+    visited = createVisitedArray();
+    clickedColor = get(x, y);
+
+    function flood(posX, posY) {
+        // check if we in the bounds of the canvas
+        if (posX < 0 || posX >= 800 || posY < 0 || posY >= 800)
+            return;
+
+        // check for visited pixels, if it is visited, then do nothing
+        if (visited[posX][posY] === true) return;
+
+        // mark the pixel as visited
+        visited[posX][posY] = true;
+
+
+        // check if the pixel is the same color as the clicked pixel
+        if (!isSameColor(clickedColor, get(posX, posY)))
+            return
+
+        set(posX, posY, fillColor)
+
+        // recursively call flood on the surrounding pixels
+        flood(posX + 1, posY); // right
+        flood(posX - 1, posY); // left
+        flood(posX, posY + 1); // down
+        flood(posX, posY - 1); // up
+    }
+
+    // start flooding
+    flood(x, y);
+
+    // reflect color changes that we made on the canvas
+    updatePixels();
+}
+
